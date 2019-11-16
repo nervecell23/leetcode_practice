7065a80ac649a27244dfa14521b847de6055433d
Chuan Cheng <c_chuan23@hotmail.com>
2019-11-16 18:30:45 +0000

    refactor solution for 'Pascal triangle problem'

--- pascal_triangle_py/solution.py
+++ pascal_triangle_py/solution.py
@@ -1,20 +1,14 @@
 class PascalTriangle:
-
     def perform(self, numRows):
-
         triangle = [[1]];
 
         for i in range(0, numRows-1):
-
             previous_row = triangle[-1]
-            current_row = []
-            current_row.append(1)
-            for j in range(0, i + 1 - 2 + 1):
-                current_element = previous_row[j] + previous_row[j+1]
-                current_row.append(current_element)
+            current_row = [1]
+            for j in range(0, i):
+                current_row.append(previous_row[j] + previous_row[j+1])
 
             current_row.append(1)
-
             triangle.append(current_row)
 
         return triangle
